8c6b9c3889dd8dbdbc95f96211d415ad29309715
omotayo-01 <oooladokun27@student.lautech.edu.ng>
2026-07-15 14:29:53 +0100

    added sca.py

---
+++ verifier/sca.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+def verify_microsoft() -> dict[str, Any]:
+    candidate: dict[str, str] = {
+         "candidate_name": "Akerele Idowu",
+        "certificate_name": "Salesforce Certified Administrator",
+        "issuing_body": "Scaleforce",
+        "credential_id": "SCA03032954",
+        "issue_date": "2018-05-10",
+        "expiry_date": "present"
+    }
+
+    verification_found = True
+
+    result: dict[str, Any] = {
+        "verificationResult": {
+            "claimType": "certification",
+            "status": "verified" if verification_found else "not_found",
+            "confidenceScore": 99,
+            "candidateClaim": candidate
+    }
+    }
+
+    return result
